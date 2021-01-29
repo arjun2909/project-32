50fb1dad35bf3c2a3961584e22948bff2d5a6872
arjun2909 <73339344+arjun2909@users.noreply.github.com>
2021-01-29 15:19:32 +0700

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,33 @@
+class Box  {
+    constructor(x,y,width,height){
+      
+      this.image = loadImage("sprites/wood1.png");
+    }
+
+
+  //level 1
+    box1=createSprite(120,250,30,40);
+    box2=createSprite(150,250,30,40);
+    box3=createSprite(180,250,30,40);
+    box4=createSprite(210,250,30,40);
+    box5=createSprite(240,250,30,40);
+    box6=createSprite(270,250,30,40);
+    box7=createSprite(300,250,30,40);
+  //level 2
+  box8=createSprite(330,235,30,40);
+  box9=createSprite(360,235,30,40);
+  box10=createSprite(390,235,30,40);
+  box11=createSprite(420,235,30,40);
+  box12=createSprite(450,235,30,40);
+  //level 3
+  box13=createSprite(360,190,30,40);
+  box14=createSprite(390,190,30,40);
+  box15=createSprite(420,190,30,40);
+  //top
+  box16=createSprite(390,155,30,40);
+  
+  drawSprites()
+
+
+}
+  
---
+++ sketch.js
@@ -0,0 +1,13 @@
+var hexagon,ground,stand,box1,box2,box3,box4,box5,box6,box7,box8,box9;
+var box10,box11,box12,box13,box14,box15,box16;
+
+
+function Preload(){
+
+}
+
+
+function draw(){
+
+}
+
